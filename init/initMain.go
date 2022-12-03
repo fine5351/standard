ab0da7e08ae24b956f787afdb2e48b50644ff63c
erickuo <eric0970569800@gmail.com>
2022-12-03 10:20:48 +0800

    2022/12/03-gin init

--- init/initMain.go
+++ init/initMain.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	r := gin.Default()
 
-	fmt.Printf("%v\n", "Hello World")
+	// 定義一個路由，用於處理GET請求
+	r.GET("/hello", func(c *gin.Context) {
+		// 從請求參數中獲取名稱
+		name := c.Query("name")
+		// 向客戶端返回一個Hello World!字符串
+		c.String(200, "Hello, %s!", name)
+	})
 
-	//SyncMapNote()
-	PrintFibonacci(10)
-
+	// 啟動Web服務，監聽在8080端口
+	r.Run(":8080")
 }
